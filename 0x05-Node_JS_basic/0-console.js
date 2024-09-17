6e4feb4c256059139dd5e2fed6e2347c626a6a15
AdhamKhaledDev <adhamk109@outlook.com>
2024-09-18 00:48:22 +0300

    Display_Message

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Adham Khaled <https://github.com/AdhamKhaledDev>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
